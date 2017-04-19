c56b6857ca19140aaf21c4c0daaf6deea7ad11c0
Matt Billock <matt@backand.com>
2017-04-19 15:33:46 -0700

    removed keys

---
+++ handler.js
@@ -0,0 +1,21 @@
+var backandCallback = require('./index.js').backandCallback;
+
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    try {
+        backandCallback(event.dbRow, event.parameters, event.userProfile, function (error, result) {
+            if (error)
+            {
+                context.fail(typeof error === 'object' ? JSON.stringify(error, null, 2) : error);
+            }
+            else{
+                context.succeed(result);
+            }
+        })
+    }
+    catch(error){
+        context.fail("unhandled: " + error);
+
+    }
+};
+
