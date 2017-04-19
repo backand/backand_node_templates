1e56a16e6e85d065068974838987c665a6e52c8a
Matt Billock <matt@backand.com>
2017-04-19 15:36:05 -0700

    fixed repo

--- twilio_node_template/index.js
+++ twilio_node_template/index.js
@@ -3,8 +3,8 @@
 var twilio = require('twilio');
 
 exports.backandCallback = function(dbRow, parameters, userProfile, respondToBackand) {
-  var accountSid = 'AC818530b828295a02fcb3c00c38774178'; // Your Account SID from www.twilio.com/console
-  var authToken = '46a1e804c9990772c4eba645fce4365e';   // Your Auth Token from www.twilio.com/console
+  var accountSid = 'YOUR_ACCOUNT_SID'; // Your Account SID from www.twilio.com/console
+  var authToken = 'YOUR_AUTH_TOKEN';   // Your Auth Token from www.twilio.com/console
 
   var twilio = require('twilio');
   var client = new twilio.RestClient(accountSid, authToken);
@@ -20,7 +20,7 @@ exports.backandCallback = function(dbRow, parameters, userProfile, respondToBack
     client.messages.create({
         body: parameters.message,
         to: parameters.to,  // Text this number
-        from: '+12248033457' // From a valid Twilio number
+        from: 'YOUR_TWILIO_NUMBER' // From a valid Twilio number
     }, function(err, message) {
         if(err)
         {
